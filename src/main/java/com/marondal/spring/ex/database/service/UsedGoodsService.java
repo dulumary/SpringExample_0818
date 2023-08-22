6f17aefa4551997bd9eb35d7433c50dff0ba136e
meta it <meta.it>
2023-08-22 18:50:42 +0900

    [김인규]  - mvc 과정 중

---
+++ src/main/java/com/marondal/spring/ex/database/service/UsedGoodsService.java
@@ -0,0 +1,19 @@
+package com.marondal.spring.ex.database.service;
+
+import org.springframework.stereotype.Service;
+
+import com.marondal.spring.ex.database.repository.UsedGoodsRepository;
+
+// 로직 (business) 담당
+@Service
+public class UsedGoodsService {
+	
+	
+	
+	// used_goods 모든 데이터 조회
+	public getUsedGoodsList() {
+		
+		repository.selectUsedGoodsList();
+	}
+
+}
