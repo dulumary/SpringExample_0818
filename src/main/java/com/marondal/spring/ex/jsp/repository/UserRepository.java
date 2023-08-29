2a925d143433c71f93bc2cd12411a1ed2e17b40c
qkrwhdgh24 <jh.corder.ssam@gmail.com>
2023-08-29 17:51:24 +0900

    [김인규]  - useGeneratedKeys 예제 진행

--- src/main/java/com/marondal/spring/ex/jsp/repository/UserRepository.java
+++ src/main/java/com/marondal/spring/ex/jsp/repository/UserRepository.java
@@ -13,6 +13,10 @@ public interface UserRepository {
 			, @Param("birthday") String birthday
 			, @Param("email") String email);
 	
+	public int insertUserByObject(User user);
+	
 	public User selectLastUser();
+	
+	
 
 }
