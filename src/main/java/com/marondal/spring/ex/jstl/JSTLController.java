013f7432b6cc0ee1596d03fa3cdb171e0009c3dc
qkrwhdgh24 <jh.corder.ssam@gmail.com>
2023-08-31 18:43:02 +0900

    [김인규]  - jstl core format 라이브러리 예제

---
+++ src/main/java/com/marondal/spring/ex/jstl/JSTLController.java
@@ -0,0 +1,72 @@
+package com.marondal.spring.ex.jstl;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@RequestMapping("/jstl")
+@Controller
+public class JSTLController {
+	
+	@GetMapping("/ex01")
+	public String ex01() {
+		return "jstl/ex01";
+	}
+	
+	@GetMapping("/ex02")
+	public String ex02(Model model) {
+		
+		List<String> fruitList = new ArrayList<>();
+		fruitList.add("사과");
+		fruitList.add("귤");
+		fruitList.add("바나나");
+		
+		model.addAttribute("fruitList", fruitList);
+		
+		// 사용자 정보 리스트 
+		// 이름, 나이, 취미
+		
+		List<Map<String, Object>> userList = new ArrayList<>();
+		
+		Map<String, Object> userMap = new HashMap<>();	
+		userMap.put("name", "김인규");
+		userMap.put("age", 29);
+		userMap.put("hobby", "독서");
+		userList.add(userMap);
+		
+		userMap = new HashMap<>();
+		userMap.put("name", "김바다");
+		userMap.put("age", 5);
+		userMap.put("hobby", "사냥하기");
+		userList.add(userMap);
+		
+		
+		model.addAttribute("userList", userList);
+		
+		
+		return "jstl/ex02";
+	}
+	
+	@GetMapping("/ex03")
+	public String ex03(Model model) {
+		
+		Date now = new Date();
+		
+		model.addAttribute("now", now);
+		
+		String dateString = "2023/08/31 17:52:23";
+		model.addAttribute("dateString", dateString);
+		
+		
+		return "jstl/ex03";
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/jsp/jstl/ex02.jsp
@@ -0,0 +1,69 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>    
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>JSTL 제어문</title>
+</head>
+<body>
+
+	<h2>조건문, 반복문</h2>
+	
+	<h3>1. if else if</h3>
+	<%-- 몸무게가 70이하면 치킨, 80이하면 샐러드, 그게 아니면 굶어  --%>
+	<c:set var="weight" value="72" />
+	
+	<c:choose>
+		<c:when test="${weight <= 70 }">
+			<h4>치킨</h4>
+		</c:when>
+		<c:when test="${weight <= 80 }">
+			<h4>샐러드</h4>
+		</c:when>
+		<c:otherwise>
+			<h4>굶어</h4>
+		</c:otherwise>
+	</c:choose>
+	
+	<h3>2. 반복문</h3>
+	
+	<%-- for(int i = 0; i < 5; i++) {} --%>
+	<c:forEach var="i" begin="0" end="4" step="1">
+		${i }
+	</c:forEach>
+	
+	<%-- for(String fruit:fruitList) { } --%>
+	<c:forEach var="fruit" items="${fruitList }" varStatus="status" >
+		<h4>${fruit } :::::  ${status.count } ${status.index } ${status.first } ${status.last } </h4>
+	</c:forEach> 
+	
+	<h3>사용자 리스트</h3>
+	
+	<table border="1">
+		<thead>
+			<tr>
+				<th>이름</th>
+				<th>나이</th>
+				<th>취미</th>
+			</tr>
+		</thead>
+		<tbody>
+			<%-- for(Map<String, Object> user:userList) {} --%>
+			<c:forEach var="user" items="${userList }"> 
+			<tr>
+				<td>${user.name }</td>
+				<td>${user.age }</td>
+				<td>${user.hobby }</td>
+			</tr>
+			</c:forEach>
+			
+		</tbody>	
+	</table>
+	
+	
+	
+
+</body>
+</html>
